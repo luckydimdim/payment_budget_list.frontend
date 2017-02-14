9b5ce8e7c4511afe08f0c8196b24c45925371752
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-02-14 16:02:20 +0300

    reorganization

--- web/main.dart
+++ web/main.dart
@@ -7,6 +7,7 @@ import 'package:angular2/router.dart';
 import 'package:angular2/src/platform/browser/location/hash_location_strategy.dart';
 import 'package:angular2/platform/common.dart';
 
+import 'package:master_layout/master_layout_component.dart';
 import 'package:alert/alert_service.dart';
 
 import 'package:payment_budget_list/payment_budget_list_component.dart';
@@ -15,12 +16,18 @@ bool get isDebug =>
     (const String.fromEnvironment('PRODUCTION', defaultValue: 'false')) !=
     'true';
 
+@Component(selector: 'app')
+@View(
+  template: '<master-layout><payment-budget-list></payment-budget-list></master-layout>',
+  directives: const [MasterLayoutComponent, PaymentBudgetListComponent])
+class AppComponent {}
+
 main() async {
   if (isDebug) {
     reflector.trackUsage();
   }
 
-  ComponentRef ref = await bootstrap(PaymentBudgetListComponent, [
+  ComponentRef ref = await bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     const Provider(LocationStrategy, useClass: HashLocationStrategy),
     const Provider(AlertService)
